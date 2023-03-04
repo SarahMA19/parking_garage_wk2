ed264d620d01a117e3c1c0356e1f38c356210104
SarahMA19 <sarahgreisen@gmail.com>
2023-03-03 21:33:24 -0800

    more updates/not sure about dictionary

--- parking_garage.py
+++ parking_garage.py
@@ -1,84 +1,85 @@
+
 class ParkingGarage():
     """
-    class ParkingGarage ....
+    What represents a parking garage
     
     """
-    def __init__(self):
-        self.parkspaces = {
-            1 :{
-            'paid' : False,
-            'open' : True,
-            'left?' : False
-            },
-            2 :{
-            'paid' : False,
-            'open' : True,
-            'left?' : False
-            },
-            3: {
-            'paid' : False,
-            'open' : True,
-            'left?' : False
-            },
-            4: {
-            'paid' : False,
-            'open' : True,
-            'left?' : False
-            },
-            5 : {
-            'paid' : False,
-            'open' : True,
-            'left?' : False
-            },
-        }
-
+    def __init__(self, ticket, spaces, pay, currentTicket, leave):
+    
+       self.ticket = 5
+       self.spaces = 5
+       self.pay = pay
+       self.currentTicket = currentTicket
+       self.leave = leave
        
 
     def takeTicket(self):
-       self.parkspaces['open'][False]
-       
-       
+         self.ticket -= 1
+         self.spaces -= 1
+         print(f'Great, here is your ticket. There are now {self.ticket} tickets & {self.spaces} spaces left') 
 
-
-        
-        #self.parkspaces{1:[2]}-= 1
-        #self.parkspaces -= 1
-        #print(f"There are now {self.tickets} available tickets!") 
-        #("This should decrease the amount of tickets available by 1 
-        #- This should decrease the amount of parkingSpaces available by 1 ")
-        #print(f"There are now {self.parkingSpaces} available parking spaces!") 
-
-    def payForParking(self):
-        self.parkspaces = input("Pay amount:")
-        print("You're ticket has been paid, you have 15 minutes to leave.")  
-        
-    def checkPaid(self):
-         self.parkspaces = ['paid'] [True]
+    def addPayment(self):
+        responce = input("Type amount you are going to pay: $")
+        self.pay.append(responce)
+              
+    def showPaid(self):
+        print("Here is your reciept for what you have paid:$")
+        for i in self.pay:
+            print(i)
                                    
     def LeaveGarage(self):
-            
-    
+        if self.currentTicket == i:
+            print("Thank you, have a nice day!")
+        elif input("You must pay for your parking before you leave, please pay now: $ "):
+            print("Thank you, have a nice day!")
+      
         
     def run(self): 
         while True:
-            responce = input('Welcome to the parking garage, there are 5 available spots. Type t for ticket, p for pay or l to leave.')
+            responce = input('Welcome to "Saronia Parking"! Type t for ticket, p for pay or l to leave.')
             
             if responce.lower() == "t":
                 self.takeTicket()
             
             if responce.lower() == "p":
-                 self.payForParking()
+                 self.addPayment()
+                 self.showPaid()
 
             if responce.lower() == "l":
-                 self.checkPaid(self)
                  self.LeaveGarage()
-                 
                  break
                  
 
-            
-        
-        
-Car = ParkingGarage()
+    
+Car = ParkingGarage(0,0,[],0, 0)
 Car.run()
 
+
+# def __init__(self):
+#         self.parkspaces = {
+#             1 :{
+#             'paid' : False,
+#             'open' : True,
+#             'left?' : False
+#             },
+#             2 :{
+#             'paid' : False,
+#             'open' : True,
+#             'left?' : False
+#             },
+#             3: {
+#             'paid' : False,
+#             'open' : True,
+#             'left?' : False
+#             },
+#             4: {
+#             'paid' : False,
+#             'open' : True,
+#             'left?' : False
+#             },
+#             5 : {
+#             'paid' : False,
+#             'open' : True,
+#             'left?' : False
+#             },
+#         }
